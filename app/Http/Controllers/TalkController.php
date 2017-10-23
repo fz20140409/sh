08a29b01d6729fe74799c7b4a0b9c06cebdf6a95
fangqy <fz20140409@163.com>
2017-10-23 16:34:54 +0800

    对我说

--- app/Http/Controllers/TalkController.php
+++ app/Http/Controllers/TalkController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Base\BaseController;
 
-class TalkController extends Controller
+class TalkController extends BaseController
 {
     //对我说
     function talkMe(){
     	
     	  return view('talk.talkMe');
     }
+
+
+
+    //获取平台有话说角色接口
+    function getLginInfo(){
+        $info=session('user');
+        if($info){
+            return response()->json(['uid'=>$info->uid,'token'=>$info->imtoken]);
+        }else{
+            return response()->json(array());
+        }
+
+    }
+
+
 }
